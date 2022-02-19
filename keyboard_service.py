46a3bcc43d5a416b4c7cb44cf0f871ba33ceddd4
Luckysandwich7 <90877920+Luckysandwich7@users.noreply.github.com>
2022-02-19 15:42:53 -0700

    Update keyboard_service.py

--- keyboard_service.py
+++ keyboard_service.py
@@ -42,13 +42,13 @@ May need to remove Up and Down key options. Double chekcing with Professor
         
         if pyray.is_key_down(pyray.KEY_RIGHT):
             dx = 1
-        
+ """       
         if pyray.is_key_down(pyray.KEY_UP):
             dy = -1
         
         if pyray.is_key_down(pyray.KEY_DOWN):
             dy = 1
-
+"""
         direction = Point(dx, dy)
         direction = direction.scale(self._cell_size)
         
