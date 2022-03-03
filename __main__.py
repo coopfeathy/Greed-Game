7bfe3bcec196616c76e782e5d686bd06e7975e37
datadigitalshredder <hov21003@byui.edu>
2022-03-03 16:14:23 +0200

    greed game

--- __main__.py
+++ __main__.py
@@ -24,6 +24,9 @@ ROWS = 40
 CAPTION = "Greed: Gems & Stones"
 DATA_PATH = os.path.dirname(os.path.abspath(__file__)) + "/messages.txt"
 WHITE = Color(255, 255, 255)
+GREEN = Color(255, 233, 0)
+BLUE = Color(0, 191, 191)
+ORANGE = Color(255, 98, 15)
 DEFAULT_ARTIFACTS = 40
 
 
@@ -42,42 +45,82 @@ def main():
     
     # create the robot
     x = int(MAX_X / 2)
-    y = int(MAX_Y / 2)
+    y = int(MAX_Y - 15)
     position = Point(x, y)
 
     robot = Actor()
     robot.set_text("#")
     robot.set_font_size(FONT_SIZE)
-    robot.set_color(WHITE)
+    robot.set_color(GREEN)
     robot.set_position(position)
     cast.add_actor("robots", robot)
-    
-    # create the artifacts
-    with open(DATA_PATH) as file:
-        data = file.read()
-        messages = data.splitlines()
 
+    # create the gem
     for n in range(DEFAULT_ARTIFACTS):
-        text = chr(random.randint(33, 126))
-        message = messages[n]
-
         x = random.randint(1, COLS - 1)
         y = random.randint(1, ROWS - 1)
         position = Point(x, y)
         position = position.scale(CELL_SIZE)
 
-        r = random.randint(0, 255)
-        g = random.randint(0, 255)
-        b = random.randint(0, 255)
-        color = Color(r, g, b)
+        # r = random.randint(0, 255)
+        # g = random.randint(0, 255)
+        # b = random.randint(0, 255)
+        # color = Color(BLUE)
         
         artifact = Artifact()
-        artifact.set_text(text)
+        artifact.set_text("*")
         artifact.set_font_size(FONT_SIZE)
-        artifact.set_color(color)
+        artifact.set_color(BLUE)
         artifact.set_position(position)
-        artifact.set_message(message)
+        # artifact.set_message(message)
         cast.add_actor("artifacts", artifact)
+    
+    # create the stone
+    for n in range(DEFAULT_ARTIFACTS):
+        x = random.randint(1, COLS - 1)
+        y = random.randint(1, ROWS - 1)
+        position = Point(x, y)
+        position = position.scale(CELL_SIZE)
+
+        # r = random.randint(0, 255)
+        # g = random.randint(0, 255)
+        # b = random.randint(0, 255)
+        # color = Color(ORANGE)
+        
+        artifact = Artifact()
+        artifact.set_text("O")
+        artifact.set_font_size(FONT_SIZE)
+        artifact.set_color(ORANGE)
+        artifact.set_position(position)
+        # artifact.set_message(message)
+        cast.add_actor("artifacts", artifact)
+
+    # create the artifacts
+    # with open(DATA_PATH) as file:
+    #     data = file.read()
+    #     messages = data.splitlines()
+
+    # for n in range(DEFAULT_ARTIFACTS):
+    #     text = chr(random.randint(33, 126))
+    #     message = messages[n]
+
+    #     x = random.randint(1, COLS - 1)
+    #     y = random.randint(1, ROWS - 1)
+    #     position = Point(x, y)
+    #     position = position.scale(CELL_SIZE)
+
+    #     r = random.randint(0, 255)
+    #     g = random.randint(0, 255)
+    #     b = random.randint(0, 255)
+    #     color = Color(r, g, b)
+        
+    #     artifact = Artifact()
+    #     artifact.set_text(text)
+    #     artifact.set_font_size(FONT_SIZE)
+    #     artifact.set_color(color)
+    #     artifact.set_position(position)
+    #     artifact.set_message(message)
+    #     cast.add_actor("artifacts", artifact)
     
     # start the game
     keyboard_service = KeyboardService(CELL_SIZE)
